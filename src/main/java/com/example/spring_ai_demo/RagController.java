a607d214b22b1c5a2de8d4920b920c5978faffaa
Sven Olding <sven.olding@vwgis.de>
2025-07-29 13:34:42 +0200

    add similarity search for the vector store

--- src/main/java/com/example/spring_ai_demo/RagController.java
+++ src/main/java/com/example/spring_ai_demo/RagController.java
@@ -1,6 +1,9 @@
 package com.example.spring_ai_demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,10 @@ public class RagController {
     @PostMapping("/load-data")
     public void loadData() {
         ragService.loadData();
+    }
+
+    @PostMapping("/similarity-search")
+    public ResponseEntity<String> similaritySearch(@RequestBody String prompt) {
+        return new ResponseEntity<String>(ragService.similaritySearch(prompt), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/spring_ai_demo/RagService.java
+++ src/main/java/com/example/spring_ai_demo/RagService.java
@@ -1,16 +1,16 @@
 package com.example.spring_ai_demo;
 
+import java.util.List;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class RagService {
@@ -18,8 +18,7 @@ public class RagService {
     private final ResourceLoader resourceLoader;
     private final JdbcTemplate jdbcTemplate;
 
-    public RagService(
-            VectorStore vectorStore, ResourceLoader resourceLoader, JdbcTemplate jdbcTemplate) {
+    public RagService(VectorStore vectorStore, ResourceLoader resourceLoader, JdbcTemplate jdbcTemplate) {
         this.vectorStore = vectorStore;
         this.resourceLoader = resourceLoader;
         this.jdbcTemplate = jdbcTemplate;
@@ -27,12 +26,22 @@ public class RagService {
 
     public void loadData() {
         jdbcTemplate.execute("DELETE FROM vector_store");
-        Resource holgerManualPdf =
-                resourceLoader.getResource("classpath:Anwendungshandbuch_Holger.pdf");
+        Resource holgerManualPdf = resourceLoader.getResource("classpath:Anwendungshandbuch_Holger.pdf");
         var documentReader = new TikaDocumentReader(holgerManualPdf);
         List<Document> documents = documentReader.get();
-        TextSplitter textSplitter = new TokenTextSplitter();
+        TextSplitter textSplitter = new TokenTextSplitter(300, 120, 5, 1000, true);
         List<Document> splitDocuments = textSplitter.apply(documents);
         vectorStore.add(splitDocuments);
+    }
+
+    public String similaritySearch(String prompt) {
+        var sb = new StringBuilder();
+        List<Document> documents = vectorStore.similaritySearch(
+                SearchRequest.builder().similarityThreshold(0.6d).query(prompt).build());
+        if (documents == null) {
+            return "";
+        }
+        documents.forEach(document -> sb.append(document.getFormattedContent()));
+        return sb.toString();
     }
 }
